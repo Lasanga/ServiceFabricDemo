38444b7eeb2c2605310a065fe51f5d55fa7f4df3
Lasanga Guruge <lasangaguruge@gmail.com>
2019-05-25 03:17:36 +0530

    Initial

---
+++ DemoFabricApplication/DemoFabricApplication.Customer/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoFabricApplication.Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [Route("GetSample")]
+        public List<string> GetSample()
+        {
+            List<string> sample = new List<string>
+            {
+                Environment.MachineName
+            };
+
+            return sample;
+        }
+    }
+}
---
+++ DemoFabricApplication/DemoFabricApplication.Shop/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoFabricApplication.Shop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        [Route("GetSample")]
+        public List<string> GetSample()
+        {
+            List<string> sample = new List<string>
+            {
+                Environment.MachineName
+            };
+
+            return sample;
+        }
+    }
+}
